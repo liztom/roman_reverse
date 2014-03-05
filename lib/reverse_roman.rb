68cdb8ed6181043c3fab0a5abfd98cc275aaaac7
Robin Miller & Liz Tom <student@epicodus.com>
2014-03-05 15:11:10 -0800

    added error handling using to_roman gem

--- lib/reverse_roman.rb
+++ lib/reverse_roman.rb
@@ -1,17 +1,22 @@
-def reverse_roman(numeral)
-  numeral_values = {'I' => 1, 'V' => 5, 'X' => 10, 'L' => 50, 'C' => 100, 'D' => 500, 'M' => 1000}
-  result = 0
-  characters = numeral.split('')
+require ('to_roman')
+  def reverse_roman(numeral)
+    numeral_values = {'I' => 1, 'V' => 5, 'X' => 10, 'L' => 50, 'C' => 100, 'D' => 500, 'M' => 1000}
+    result = 0
+    characters = numeral.split('')
+    last_index = characters.length - 1
 
-  characters.each_with_index do |character, index|
-    if index < (characters.length - 1) && numeral_values[character] < numeral_values[characters[index+1]]
-      value = numeral_values[character] * -1
-    else
-      value = numeral_values[character]
+    characters.each_with_index do |character, index|
+      if index < last_index && numeral_values[character] < numeral_values[characters[index+1]]
+        value = numeral_values[character] * -1
+      else
+        value = numeral_values[character]
+      end
+      result += value
     end
-    result += value
+    raise ArgumentError, 'Entered roman numeral is not valid' unless result.to_roman == numeral
+    result
   end
-  result
-end
 
-puts reverse_roman('CDMLIIID')
+
+
+puts reverse_roman('MMXIV')
